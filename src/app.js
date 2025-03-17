36243c63505544114fa634f9db21f0df36c852cd
Your Name <you@example.com>
2025-03-17 23:47:26 +0530

    API level data sanitazation

--- src/app.js
+++ src/app.js
@@ -18,11 +18,71 @@ app.post("/signup",async (req, res) => {
     try{
     await user.save();
     res.send("User added succesfully!");
-} catch(err){
+    } catch(err){
     res.status(400).send("Error saving user: " + err.message);
 }
 });
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
 
+    try{
+        const user = await User.findOne({emailId: userEmail});
+        if (!user) {
+         return res.status(404).send("User not found.");
+        }else{
+        res.send(user);
+    }
+    } catch(err){
+    res.status(500).send("Something went wrong");
+    }
+});
+app.get("/feed", async (req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    } catch(err){
+    res.status(500).send("Something went wrong");
+    }
+});
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        if (!user) {
+            return res.status(404).send("User not found.");
+        }else{
+            res.send("User deleted successfully.");
+        }
+    } catch(err){
+        res.status(500).send("Something went wrong");
+        }
+});  
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId;
+    const data = req.body;
+
+    const ALLOWED_UPDATES = [
+        "userId","photoUrl", "about","gender","age","skills"
+    ]
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+     (ALLOWED_UPDATES).includes(k)
+    );
+    if(!isUpdateAllowed) {
+        res.status(403).send("Invalid");
+    }
+
+    try{
+        await User.findByIdAndUpdate( {_id: userId}, data, {
+            returnDocument: "after",
+            runValidators: true,
+    });
+            console.log(userId);
+            res.send("User updated successfully.");
+    }catch(err){
+        res.status(500).send("Something went wrong");
+        }
+    });
 
 connectDB()
 .then(() =>{
--- src/models/user.js
+++ src/models/user.js
@@ -3,22 +3,47 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
     },
     LastName: {
         type: String,
     },
     emailId: {
         type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
     },
     password: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
+        min: 18,
     },
     gender: {
         type: String,
+        validate(value){
+            if(!["Male", "Female", "Others"].includes(value)){
+                throw new Error("Gender should be either Male, Female or Other");
+            }
+        }
     },
+    photoUrl: {
+        type: String,
+        default: "https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg"
+    },
+    about: {
+        type: String,
+        default:"This is the default value of the user!",
+    },
+    skills: {
+        type: [String],
+    },
+},{
+    timestamps: true,
 });
 
 module.exports = mongoose.model("User",userSchema);
